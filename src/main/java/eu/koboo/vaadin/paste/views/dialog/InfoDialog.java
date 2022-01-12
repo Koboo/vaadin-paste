2d328076874f6a37c93a567dfad8c6e3999b8db2
Koboo <14143908+Koboo@users.noreply.github.com>
2022-01-12 16:02:10 +0100

    Optimize InfoDialog

--- src/main/java/eu/koboo/vaadin/paste/views/dialog/InfoDialog.java
+++ src/main/java/eu/koboo/vaadin/paste/views/dialog/InfoDialog.java
@@ -3,6 +3,7 @@ package eu.koboo.vaadin.paste.views.dialog;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.html.Span;
@@ -19,18 +20,17 @@ public class InfoDialog extends Dialog {
     title.getStyle().set("margin", "unset");
     add(title);
 
-    add(new Paragraph("Lightweight Paste-Service with Ace-Editor"));
-    add(new Paragraph("Some Hotkeys:"));
-    add(new Paragraph(new Span("Save a paste"), new Badge("CTRL + S")));
-    add(new Paragraph(new Span("Create new paste"), new Badge("CTRL + S")));
-    add(new Paragraph(new Span("Open info"), new Badge("CTRL + I")));
-    add(new Paragraph(new Span("Open settings"), new Badge("CTRL + ALT + I")));
+    add(new Paragraph("Lightweight PasteService with Vaadin and MongoDB"));
+    add(new Paragraph(new Span("Save a paste  "), new Badge("CTRL + S")));
+    add(new Paragraph(new Span("Create new paste  "), new Badge("CTRL + S")));
+    add(new Paragraph(new Span("Open info  "), new Badge("CTRL + I")));
+    add(new Paragraph(new Span("Open settings  "), new Badge("CTRL + ALT + I")));
     add(new Paragraph(""));
     add(new Paragraph(
         "Please note: The created pastes are only stored temporarily and will be deleted after 1 week. This can be adjusted when installing your own instance!"));
     add(new Paragraph(""));
     add(new Paragraph("Built by Koboo with <3"));
-    add(new Paragraph("https://github.com/Koboo/vaadin-paste"));
+    add(new Paragraph(new Anchor("https://github.com/Koboo/vaadin-paste")));
     add(new Paragraph("Feel free to leave a star. Contributions always welcome!"));
 
     Button closeButton = new Button("Close", VaadinIcon.CLOSE.create());
