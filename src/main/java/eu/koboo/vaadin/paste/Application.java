d3def877879e48bc16fc96ac14522b7e158f65d0
Koboo <14143908+Koboo@users.noreply.github.com>
2022-01-12 13:56:13 +0100

    Intial commit

--- src/main/java/eu/koboo/vaadin/paste/Application.java
+++ src/main/java/eu/koboo/vaadin/paste/Application.java
@@ -4,25 +4,25 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * The entry point of the Spring Boot application.
- *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- *
+ * <p>
+ * Use the @PWA annotation make the application installable on phones, tablets and some desktop
+ * browsers.
  */
 @SpringBootApplication
-@Theme(value = "vaadinpaste")
+@Theme(value = "vaadinpaste", variant = Lumo.DARK)
 @PWA(name = "Vaadin Paste", shortName = "Vaadin Paste", offlineResources = {"images/logo.png"})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
