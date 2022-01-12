aae372ac74653d8bdf9aa10954c50459eb0300e5
Koboo <14143908+Koboo@users.noreply.github.com>
2022-01-12 13:58:35 +0100

    Create Viewless Clipboard utility

---
+++ src/main/java/eu/koboo/vaadin/paste/utility/Clipboard.java
@@ -0,0 +1,49 @@
+package eu.koboo.vaadin.paste.utility;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import eu.koboo.vaadin.paste.repository.Paste;
+
+public class Clipboard {
+
+  public Clipboard() {
+    String javaScriptFunction = "window.copyToClipboard = (str) => {\n"
+        + "  const textarea = document.createElement(\"textarea\");\n"
+        + "  textarea.value = str;\n"
+        + "  textarea.style.position = \"absolute\";\n"
+        + "  textarea.style.opacity = \"0\";\n"
+        + "  document.body.appendChild(textarea);\n"
+        + "  textarea.select();\n"
+        + "  document.execCommand(\"copy\");\n"
+        + "  document.body.removeChild(textarea);\n"
+        + "};";
+    UI.getCurrent().getPage().executeJs(javaScriptFunction);
+  }
+
+  public void copyTo(String value) {
+    UI.getCurrent().getPage().executeJs("window.copyToClipboard($0)", value);
+  }
+
+  public void copyCode(Paste paste, Runnable callback) {
+    UI.getCurrent().getPage().fetchCurrentURL(url -> {
+      String domain;
+      if(url.getHost().equalsIgnoreCase("localhost")) {
+        domain = url.getProtocol() + "://" + url.getHost() + ":" + url.getPort() + "/";
+      } else {
+        domain = url.getProtocol() + "://" + url.getHost() + "/";
+      }
+      copyTo(domain + "show?p=" + paste.getPasteId());
+
+      Notification n = new Notification();
+      n.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+      n.setText("Copied to Clipboard!");
+      n.setDuration(2500);
+      n.open();
+      if(callback != null) {
+        callback.run();
+      }
+    });
+  }
+
+}
