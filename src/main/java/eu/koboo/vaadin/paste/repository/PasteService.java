0aad966453cc2dfd5ca059580ffbc18a9b2a179b
Koboo <14143908+Koboo@users.noreply.github.com>
2022-01-12 16:22:42 +0100

    Fix PasteService clear logic error

--- src/main/java/eu/koboo/vaadin/paste/repository/PasteService.java
+++ src/main/java/eu/koboo/vaadin/paste/repository/PasteService.java
@@ -6,11 +6,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 public class PasteService {
 
   private static final Logger logger = Logger.getLogger(PasteService.class.getName());
@@ -18,20 +18,36 @@ public class PasteService {
   @Getter
   PasteRepository repository;
 
+  @Value("${paste.days}")
+  private String pasteDays;
+
+  public PasteService(PasteRepository repository) {
+    this.repository = repository;
+  }
+
   @Scheduled(fixedRate = 86_400_000)
   public void execute() {
-    LocalDate now = LocalDate.now();
-    int deletion = 0;
-    int total = 0;
-    for (Paste paste : repository.findAll()) {
-      total += 1;
-      LocalDate validUntilDate = LocalDate.parse(paste.getCreatedAt(), Date.FORMATTER).plusDays(7);
-      if(validUntilDate.isAfter(now)) {
-        repository.delete(paste);
-        deletion += 1;
+    try {
+      logger.log(Level.INFO,
+          "clear routine started..");
+      int pasteDaysInt = Integer.parseInt(pasteDays);
+      LocalDate now = LocalDate.now();
+      int deletion = 0;
+      int total = 0;
+      for (Paste paste : repository.findAll()) {
+        total += 1;
+        LocalDate validUntilDate = LocalDate.parse(paste.getCreatedAt(), Date.FORMATTER)
+            .plusDays(pasteDaysInt);
+        if (validUntilDate.isBefore(now)) {
+          repository.delete(paste);
+          deletion += 1;
+        }
       }
+      logger.log(Level.INFO,
+          "clear routine deleted " + deletion + " of " + total + " total pastes..");
+    } catch (NumberFormatException e) {
+      logger.log(Level.WARNING, "couldn't execute clear routine. property \"paste.days\" is wrong! (value=\"" + pasteDays + "\", must be integer!");
     }
-    logger.log(Level.INFO, "clear routine deleted " + deletion + " of " + total + " total pastes..");
   }
 
 }
